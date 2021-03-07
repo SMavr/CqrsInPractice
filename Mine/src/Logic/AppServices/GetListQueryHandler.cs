039322884ca606ba571a3b4d6a3b1faa257713c6
smavrikis <smavrikis@BNFT-WEBPC6>
2021-03-07 11:11:27 +0200

    Removed convert to method

--- Mine/src/Logic/AppServices/GetListQueryHandler.cs
+++ Mine/src/Logic/AppServices/GetListQueryHandler.cs
@@ -45,22 +45,6 @@ namespace Logic.AppServices
                 return null;
             }
         }
-
-        private StudentDto ConvertToDto(Student student)
-        {
-            return new StudentDto
-            {
-                Id = student.Id,
-                Name = student.Name,
-                Email = student.Email,
-                Course1 = student.FirstEnrollment?.Course?.Name,
-                Course1Grade = student.FirstEnrollment?.Grade.ToString(),
-                Course1Credits = student.FirstEnrollment?.Course?.Credits,
-                Course2 = student.SecondEnrollment?.Course?.Name,
-                Course2Grade = student.SecondEnrollment?.Grade.ToString(),
-                Course2Credits = student.SecondEnrollment?.Course?.Credits,
-            };
-        }
     }
 
     public class StudentInDb
